30320bad2a6dff54700cac17335aad43b16c22cc
Gabriel Yuji Yasuda <gabrielyyasuda@gmail.com>
2023-07-19 21:29:51 -0300

    Initial commit

---
+++ TaskManagerMVC/Controllers/TaskController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManagerMVC.Data;
+using TaskManagerMVC.Models;
+
+namespace TaskManagerMVC.Controllers
+{
+	public class TaskController : Controller
+	{
+		//Create the DB connection
+		private readonly ApplicationDbContext _context;
+        public TaskController(ApplicationDbContext context)
+        {
+			_context = context;
+        }
+
+        public IActionResult Index()
+		{
+			List<TaskModel> taskList = _context.Tasks.ToList();
+
+			return View(taskList);
+		}
+	}
+}
---
+++ TaskManagerMVC/Models/TaskModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagerMVC.Models
+{
+	public class TaskModel
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[MaxLength(50)]
+		[Required]
+		public string Name { get; set; }
+
+		public string? Description { get; set; }
+	}
+}
